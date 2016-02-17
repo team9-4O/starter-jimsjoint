ab9e5c2f4d13a3abdc6bfeae49b3865f559c104e
imoongom <moongom.ca@gmail.com>
2016-02-16 18:14:23 -0800

    fix errors

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,12 +20,12 @@ class Welcome extends Application {
     //-------------------------------------------------------------
 
     function index() {
-		$this->load->model('orders');
+        $this->load->model('orders');
         $this->data['title'] = 'Jim\'s Joint!';
         $this->data['pagebody'] = 'welcome';
 
         // Get all the completed orders
-        $completed = $this->orders->some('statues', 'c');
+        $completed = $this->orders->some('status', 'c');
 
         // Build a multi-dimensional array for reporting
         $orders = array();
