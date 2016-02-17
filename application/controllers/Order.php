903ea01129b7c57457114e162275e7b7bf13898d
Tom Tang <Tomtang_13@hotmail.com>
2016-02-16 17:26:53 -0800

    did part 7

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -101,7 +101,14 @@ class Order extends Application {
 
     // proceed with checkout
     function commit($order_num) {
-        //FIXME
+        if (!$this->orders->validate($order_num))
+			redirect('/order/display_menu/'.$order_num);
+		$record = $this->orders->get($order_num);
+		$record->date = date(DATE_ATOM);
+		$record->status = 'c';
+		$record->total = $this->orders->total($order_num);
+		$this->orders->update($record);
+		$this->orders->update($record);
         redirect('/');
     }
 
